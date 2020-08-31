ef6667d443550c5c2c5b9497e472dea9973f9b9e
slyshot <slyshot@protonmail.com>
2020-08-31 00:42:40 -0400

    More cleaning, added "_switch" and "_compare" typedefs.

--- src/queue.c
+++ src/queue.c
@@ -1,6 +1,6 @@
 #include "queue.h"
 
-void sift_down(_UL index, queue _queue, _UL queue_len ,void (*sw)(queue, _UL, _UL, void*), void *sw_a, _UL (*comp)(queue, _UL, _UL, void*), void *c_a) {
+void sift_down(_UL index, queue _queue, _UL queue_len ,_switch sw, void *sw_a, _compare comp, void *c_a) {
 	_UL cur_index = index;
 	while (1) {
 		_UL child1 = cur_index*2 + 1;
@@ -12,21 +12,21 @@ void sift_down(_UL index, queue _queue, _UL queue_len ,void (*sw)(queue, _UL, _U
 		cur_index = best_child_index;
 	}
 }
-queue_element pop(queue _queue, _UL queue_len, void (*sw)(queue, _UL, _UL, void*), void *sw_a, _UL (*comp)(queue, _UL, _UL, void*), void *c_a) {
+queue_element pop(queue _queue, _UL queue_len, _switch sw, void *sw_a, _compare comp, void *c_a) {
 	queue_element best_element = _queue[0];
 	_queue[0] = _queue[queue_len - 1];
 	sift_down(0, _queue, queue_len - 1,sw,sw_a,comp,c_a);
 	return best_element;
 }
-void heapify(queue _queue, _UL queue_len, void (*sw)(queue,_UL, _UL, void*), void *sw_a, _UL (*comp)(queue, _UL, _UL, void*), void *c_a) {
+void heapify(queue _queue, _UL queue_len, void (*sw)(queue,_UL, _UL, void*), void *sw_a, _compare comp, void *c_a) {
 	for (int i = (queue_len - 2)/2; i >= 0; i-- ) sift_down(i,_queue,queue_len,sw, sw_a,comp, c_a);
 }
-void sift_up(_UL init_index, queue _queue, void (*sw)(queue, _UL, _UL, void*),void *sw_a, _UL (*comp)(queue, _UL, _UL, void*), void *c_a) {
+void sift_up(_UL init_index, queue _queue, _switch sw,void *sw_a, _compare comp, void *c_a) {
 	for (_UL index = init_index; index > 0; index = (index-1)/2) {
 		if (comp(_queue, (index - 1)/2,index, c_a) == index) sw(_queue,(index - 1)/2,index, sw_a);		
 	}
 }
-void push(queue _queue, queue_element item, _UL index, void (*sw)(queue, _UL, _UL, void*),void *sw_a, _UL (*comp)(queue, _UL, _UL, void*), void *c_a) {
+void push(queue _queue, _UL index, queue_element item, _switch sw,void *sw_a, _compare comp, void *c_a) {
 	_queue[index] = item;
 	sift_up(index,_queue, sw,sw_a,comp,c_a);
 }
